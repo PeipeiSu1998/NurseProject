62ff593d31c3b06a36c3f55162f5dde7a33404d0
Peipei Su <266128@via.dk>
2020-04-07 17:36:42 +0200

    main menu scene updated

---
+++ Nurse Project/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+using VRTK.Prefabs.CameraRig.UnityXRCameraRig.Input;
+
+public class MenuButtons : MonoBehaviour
+{
+    public GameObject gameObject;
+
+    public GameObject validCube;
+    public UnityButtonAction unityButtonAction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (validCube.activeSelf)
+        {
+            if (other.gameObject.CompareTag("Exist"))
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+}

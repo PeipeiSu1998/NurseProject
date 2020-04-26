1de2635082297e180ee339b7d117fea7b5bd819a
DaniKoch1 <baqueta.dani@gmail.com>
2020-04-26 18:24:54 +0200

    Addded label template for controller buttons

---
+++ Nurse Project/Assets/Scripts/LineManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject[] labels;
+    [SerializeField]
+	private GameObject[] buttons;
+
+	void Update () {
+		for(int i = 0; i < buttons.Length; i++)
+			DrawLine(labels[i].transform.position, buttons[i].transform.position, Color.black);
+    }
+	void DrawLine(Vector3 start, Vector3 end, Color color, float duration = 0.018f){
+		GameObject myLine = new GameObject();
+		myLine.transform.position = start;
+		myLine.AddComponent<LineRenderer>();
+		LineRenderer lr = myLine.GetComponent<LineRenderer>();
+		lr.material = new Material(Shader.Find("Sprites/Default"));
+		lr.SetColors(color, color);
+		lr.SetWidth(0.005f, 0.005f);
+		lr.SetPosition(0, start);
+		lr.SetPosition(1, end);
+		GameObject.Destroy(myLine, duration);
+	}
+}

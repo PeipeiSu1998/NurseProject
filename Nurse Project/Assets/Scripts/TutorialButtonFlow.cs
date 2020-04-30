b1b567067775e8b21a305c904e08002933dde0c8
Peipei Su <266128@via.dk>
2020-04-30 11:52:22 +0200

    tutorial flow initialized

---
+++ Nurse Project/Assets/Scripts/TutorialButtonFlow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialButtonFlow : MonoBehaviour
+{
+    public TutorialGameFlow tutorialGameFlow;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.name == "left_touch_controller_model_skel" || other.gameObject.name == "right_touch_controller_model_skel")
+        {
+            if (gameObject.name == "Button1" && tutorialGameFlow.snapdropzone && !tutorialGameFlow.button1)
+            {
+                tutorialGameFlow.button1 = true;
+                Debug.Log("button1 : " + tutorialGameFlow.button1 + ", button2: " + tutorialGameFlow.button2 + ", button3: " + tutorialGameFlow.button3 + ", button4: " + tutorialGameFlow.button4 + ", button5: " + tutorialGameFlow.button5);
+            }else if(gameObject.name == "Button2" && tutorialGameFlow.button1 && !tutorialGameFlow.button2)
+            {
+                tutorialGameFlow.button2 = true;
+                Debug.Log("button1 : " + tutorialGameFlow.button1 + ", button2: " + tutorialGameFlow.button2 + ", button3: " + tutorialGameFlow.button3 + ", button4: " + tutorialGameFlow.button4 + ", button5: " + tutorialGameFlow.button5);
+            }
+            else if(gameObject.name == "Button3" && tutorialGameFlow.button2 && !tutorialGameFlow.button3)
+            {
+                tutorialGameFlow.button3 = true;
+                Debug.Log("button1 : " + tutorialGameFlow.button1 + ", button2: " + tutorialGameFlow.button2 + ", button3: " + tutorialGameFlow.button3 + ", button4: " + tutorialGameFlow.button4 + ", button5: " + tutorialGameFlow.button5);
+            }
+            else if(gameObject.name == "Button4" && tutorialGameFlow.button3 && !tutorialGameFlow.button4)
+            {
+                tutorialGameFlow.button4 = true;
+                Debug.Log("button1 : " + tutorialGameFlow.button1 + ", button2: " + tutorialGameFlow.button2 + ", button3: " + tutorialGameFlow.button3 + ", button4: " + tutorialGameFlow.button4 + ", button5: " + tutorialGameFlow.button5);
+            }
+            else if(gameObject.name == "Button5" && tutorialGameFlow.button4 && !tutorialGameFlow.button5)
+            {
+                tutorialGameFlow.button5 = true;
+                Debug.Log("button1 : " + tutorialGameFlow.button1 + ", button2: " + tutorialGameFlow.button2 + ", button3: " + tutorialGameFlow.button3 + ", button4: " + tutorialGameFlow.button4 + ", button5: " + tutorialGameFlow.button5);
+            }
+        }
+    }
+}
---
+++ Nurse Project/Assets/Scripts/TutorialGameFlow.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialGameFlow : MonoBehaviour
+{
+    private bool instruction;
+    private bool teleport;
+    private bool grab;
+    public bool snapdropzone;
+    public bool button1;
+    public bool button2;
+    public bool button3;
+    public bool button4;
+    public bool button5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instruction = false;
+        teleport = false;
+        grab = false;
+        snapdropzone = false;
+        button1 = false;
+        button2 = false;
+        button3 = false;
+        button4 = false;
+        button5 = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void InstructionButtonDone()
+    {
+        if (!instruction)
+        {
+            instruction = true;
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void TeleportButtonDone()
+    {
+        if(instruction && !teleport)
+        {
+            teleport = true;
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void GrabDone()
+    {
+        if(teleport && !grab)
+        {
+            grab = true;
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void SnapDropZoneDone()
+    {
+        if(grab && !snapdropzone)
+        {
+            snapdropzone = true;
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+}

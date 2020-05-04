ab82f61bfe4f39798d51defaee9172e8165c7ee0
DaniKoch1 <baqueta.dani@gmail.com>
2020-05-04 14:33:19 +0200

    Tutorial flow

--- Nurse Project/Assets/Scripts/TutorialGameFlow.cs
+++ Nurse Project/Assets/Scripts/TutorialGameFlow.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialGameFlow : MonoBehaviour
 {
@@ -14,7 +15,8 @@ public class TutorialGameFlow : MonoBehaviour
     public bool button4;
     public bool button5;
     public GameObject sceneChanger;
-    // Start is called before the first frame update
+    public Text displayedText;
+
     void Start()
     {
         instruction = false;
@@ -26,9 +28,8 @@ public class TutorialGameFlow : MonoBehaviour
         button3 = false;
         button4 = false;
         button5 = false;
+        displayedText.text = "Please click on A or X button in your controller to show the instructions";
     }
-
-    // Update is called once per frame
     void Update()
     {
         if (instruction && teleport && grab && snapdropzone && button1 && button2 && button3 && button4 && button5)
@@ -42,6 +43,7 @@ public class TutorialGameFlow : MonoBehaviour
         if (!instruction)
         {
             instruction = true;
+            displayedText.text = "Please take a look at the instructions and try to teleport somewhere.";
 
             Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
         }
@@ -52,6 +54,7 @@ public class TutorialGameFlow : MonoBehaviour
         if(instruction && !teleport)
         {
             teleport = true;
+            displayedText.text = "Please grab the button panel next to the bed by using the grip.";
 
             Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
         }
@@ -62,6 +65,62 @@ public class TutorialGameFlow : MonoBehaviour
         if(teleport && !grab)
         {
             grab = true;
+            displayedText.text = "Well done, please put the button panel back to its place.";
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void Button1Done()
+    {
+        if(grab && !button1)
+        {
+            button1 = true;
+            displayedText.text = "Please use the trigger on your controller to click the first button. You can use it to put the patient in the Fowlers position. It can help with difficulties with breathing or dyspnoe.";
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void Button2Done()
+    {
+        if(button1 && !button2)
+        {
+            button2 = true;
+            displayedText.text = "Now click the second button. You can use it to regulate Fowlers position to a lower or higher position for the upper body. It can prevent naucea, stop nosebleeding by preventing high blood pressure in the head/nose area or help with a night cough.";
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void Button3Done()
+    {
+        if(button2 && !button3)
+        {
+            button3 = true;
+            displayedText.text = "Now click the third button. You can use it to regulate Fowlers position to a lower position for the lower body. It can ease pain in legs, so you can use it when you are giving the patient a compression on the legs.";
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void Button4Done()
+    {
+        if(button3 && !button4)
+        {
+            button4 = true;
+            displayedText.text = "Now click the fourth button. Use it for legs with oedema, it helps blod in legs to pass towards the heart which eases the pressure and prevents/eases the oedema.";
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
+    public void Button5Done()
+    {
+        if(button4 && !button5)
+        {
+            button5 = true;
+            displayedText.text = "Now click the fifth button. The bed will return to its original position.";
 
             Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
         }
@@ -69,9 +128,10 @@ public class TutorialGameFlow : MonoBehaviour
 
     public void SnapDropZoneDone()
     {
-        if(grab && !snapdropzone)
+        if(button5 && !snapdropzone)
         {
             snapdropzone = true;
+            displayedText.text = "Well done, now you can get into the test scene, try to teleport to the circle at the middle of this room.";
 
             Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
         }

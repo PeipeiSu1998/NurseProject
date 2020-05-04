82f7b0e6fc808558892ebbe732fde69ef491c6f2
Peipei Su <266128@via.dk>
2020-05-04 16:07:23 +0200

    fixed

--- Nurse Project/Assets/Scripts/TutorialGameFlow.cs
+++ Nurse Project/Assets/Scripts/TutorialGameFlow.cs
@@ -71,12 +71,23 @@ public class TutorialGameFlow : MonoBehaviour
         }
     }
 
+    public void SnapDropZoneDone()
+    {
+        if (grab && !snapdropzone)
+        {
+            snapdropzone = true;
+            displayedText.text = "Please use the trigger on your controller to click the first button. You can use it to put the patient in the Fowlers position. It can help with difficulties with breathing or dyspnoe.";
+
+            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
+        }
+    }
+
     public void Button1Done()
     {
-        if(grab && !button1)
+        if(snapdropzone && !button1)
         {
             button1 = true;
-            displayedText.text = "Please use the trigger on your controller to click the first button. You can use it to put the patient in the Fowlers position. It can help with difficulties with breathing or dyspnoe.";
+            displayedText.text = "Now click the second button. You can use it to regulate Fowlers position to a lower or higher position for the upper body. It can prevent naucea, stop nosebleeding by preventing high blood pressure in the head/nose area or help with a night cough.";
         }
     }
 
@@ -85,7 +96,7 @@ public class TutorialGameFlow : MonoBehaviour
         if(button1 && !button2)
         {
             button2 = true;
-            displayedText.text = "Now click the second button. You can use it to regulate Fowlers position to a lower or higher position for the upper body. It can prevent naucea, stop nosebleeding by preventing high blood pressure in the head/nose area or help with a night cough.";
+            displayedText.text = "Now click the third button. You can use it to regulate Fowlers position to a lower position for the lower body. It can ease pain in legs, so you can use it when you are giving the patient a compression on the legs.";
         }
     }
 
@@ -94,7 +105,7 @@ public class TutorialGameFlow : MonoBehaviour
         if(button2 && !button3)
         {
             button3 = true;
-            displayedText.text = "Now click the third button. You can use it to regulate Fowlers position to a lower position for the lower body. It can ease pain in legs, so you can use it when you are giving the patient a compression on the legs.";
+            displayedText.text = "Now click the fourth button. Use it for legs with oedema, it helps blod in legs to pass towards the heart which eases the pressure and prevents/eases the oedema.";
         }
     }
 
@@ -103,7 +114,7 @@ public class TutorialGameFlow : MonoBehaviour
         if(button3 && !button4)
         {
             button4 = true;
-            displayedText.text = "Now click the fourth button. Use it for legs with oedema, it helps blod in legs to pass towards the heart which eases the pressure and prevents/eases the oedema.";
+            displayedText.text = "Now click the fifth button. The bed will return to its original position.";
         }
     }
 
@@ -112,18 +123,9 @@ public class TutorialGameFlow : MonoBehaviour
         if(button4 && !button5)
         {
             button5 = true;
-            displayedText.text = "Now click the fifth button. The bed will return to its original position.";
+            displayedText.text = "Well done, now you can teleport to the circle at the middle of this room, to get into the test scene.";
         }
     }
 
-    public void SnapDropZoneDone()
-    {
-        if(button5 && !snapdropzone)
-        {
-            snapdropzone = true;
-            displayedText.text = "Well done, now you can get into the test scene, try to teleport to the circle at the middle of this room.";
-
-            Debug.Log("instruction: " + instruction + ", teleport: " + teleport + ", grab: " + grab + ", snapdropzone: " + snapdropzone);
-        }
-    }
+    
 }

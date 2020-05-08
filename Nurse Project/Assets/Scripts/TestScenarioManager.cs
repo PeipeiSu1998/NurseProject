749393d00c82e3f0cd2eaabcdf7b2b411ba3e803
DaniKoch1 <baqueta.dani@gmail.com>
2020-05-08 17:20:12 +0200

    Small fix

--- Nurse Project/Assets/Scripts/TestScenarioManager.cs
+++ Nurse Project/Assets/Scripts/TestScenarioManager.cs
@@ -32,38 +32,40 @@ public class TestScenarioManager : MonoBehaviour
             ActivateGameObjectForaWhile(correct);
         else if(num == -1)
             ActivateGameObjectForaWhile(incorrect);
+        if(num == 1){
         int caseNo = Random.Range(0, 6);
-        switch(caseNo){
-            case 0:
-                script = "Your patient has difficulties with breathing. Adjust the bed accordingly.";
-                ScenarioButtonInteraction.chooseName("Button1");
-                break;
-            case 1: 
-                script = "Your patient has dyspnoe. Adjust the bed accordingly.";
-                ScenarioButtonInteraction.chooseName("Button1");
-                break;
-            case 2:
-                script = "Your patient has naucea. Adjust the bed accordingly.";
-                ScenarioButtonInteraction.chooseName("Button2");
-                break;
-            case 3:
-                script = "Your patient has nosebleeding. Adjust the bed accordingly.";
-                ScenarioButtonInteraction.chooseName("Button2");
-                break;
-            case 4:
-                script = "Your patient has night cough. Adjust the bed accordingly.";
-                ScenarioButtonInteraction.chooseName("Button2");
-                break;
-            case 5:
-                script = "You need to give your patient a compression on the legs. Adjust the bed to ease the pain.";
-                ScenarioButtonInteraction.chooseName("Button3");
-                break;
-            case 6:
-                script = "Your patient has edema of the legs. Adjust the bed accordingly.";
-                ScenarioButtonInteraction.chooseName("Button4");
-                break;
+            switch(caseNo){
+                case 0:
+                    script = "Your patient has difficulties with breathing. Adjust the bed accordingly.";
+                    ScenarioButtonInteraction.chooseName("Button1");
+                    break;
+                case 1: 
+                    script = "Your patient has dyspnoe. Adjust the bed accordingly.";
+                    ScenarioButtonInteraction.chooseName("Button1");
+                    break;
+                case 2:
+                    script = "Your patient has naucea. Adjust the bed accordingly.";
+                    ScenarioButtonInteraction.chooseName("Button2");
+                    break;
+                case 3:
+                    script = "Your patient has nosebleeding. Adjust the bed accordingly.";
+                    ScenarioButtonInteraction.chooseName("Button2");
+                    break;
+                case 4:
+                    script = "Your patient has night cough. Adjust the bed accordingly.";
+                    ScenarioButtonInteraction.chooseName("Button2");
+                    break;
+                case 5:
+                    script = "You need to give your patient a compression on the legs. Adjust the bed to ease the pain.";
+                    ScenarioButtonInteraction.chooseName("Button3");
+                    break;
+                case 6:
+                    script = "Your patient has edema of the legs. Adjust the bed accordingly.";
+                    ScenarioButtonInteraction.chooseName("Button4");
+                    break;
+            }
+            text.text = script;
         }
-        text.text = script;
     }
     public IEnumerator ActivateGameObjectForaWhile(GameObject go){
         go.SetActive(true);

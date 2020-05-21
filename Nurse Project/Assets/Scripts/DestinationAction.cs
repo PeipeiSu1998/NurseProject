e7a832745228d00f4d6d4a3b1a3d83db30239244
Peipei Su <266128@via.dk>
2020-05-21 17:28:00 +0200

    destination location improved

---
+++ Nurse Project/Assets/Scripts/DestinationAction.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestinationAction : MonoBehaviour
+{
+
+    public void Tutorial()
+    {
+        SceneManager.LoadScene("Tutorial");
+    }
+
+    public void TestNormal()
+    {
+        GameModeHolder.Instance.mode = GameMode.normal;
+        SceneManager.LoadScene("Test");
+    }
+
+    public void TestSurvival()
+    {
+        GameModeHolder.Instance.mode = GameMode.survival;
+        SceneManager.LoadScene("Test");
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+}

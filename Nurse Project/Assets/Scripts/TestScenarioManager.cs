f9026607a5ddcfb3c9bae9a2714e6ea9df26db66
DaniKoch1 <baqueta.dani@gmail.com>
2020-05-22 12:31:35 +0200

    Small fix

--- Nurse Project/Assets/Scripts/TestScenarioManager.cs
+++ Nurse Project/Assets/Scripts/TestScenarioManager.cs
@@ -62,7 +62,7 @@ public class TestScenarioManager : MonoBehaviour
                     ScenarioButtonInteraction.chooseName("Button4");
                     break;
             }
-            script += "<br/>Correct: " + answerManager.GetCorrect();
+            script += "<br> Correct: " + answerManager.GetCorrect();
             text.text = script;
     }
 }
